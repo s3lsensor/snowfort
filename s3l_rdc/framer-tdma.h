d098f3d8d3d2f8723dc3c48a10e50b532b1d99e3
Yizheng Liao <liaoyizheng@gmail.com>
2013-05-14 19:35:02 -0700

    framer tdma

---
+++ s3l_rdc/framer-tdma.h
@@ -0,0 +1,20 @@
+/*
+ * 
+ * MAC framer for tdma
+ */
+
+/**
+ * \file
+ * 		Mac framer for tdma
+ * \author
+ * 		Yizheng Liao <yzliao@stanford.edu>
+ */
+
+#ifndef __FRAMER_TDMAMAC_H__
+#define __FRAMER_TDMAMAC_H__
+
+#include "net/mac/framer/h"
+
+extern const struct framer framer_tdma;
+
+#endif /* __FRAMER_TDMAMAC_H__ */
--- s3l_rdc/nullApp.c
+++ s3l_rdc/nullApp.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include "contiki.h"
+#include "net/packetbuf.h"
+
+
+#define DEBUG 0
+
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINTADDR(addr) PRINTF(" %02x%02x:%02x%02x:%02x%02x:%02x%02x ", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7])
+#else
+#define PRINTF(...)
+#define PRINTADDR(addr)
+#endif
 
 PROCESS(null_app_process, "Null App Process");
 AUTOSTART_PROCESSES(&null_app_process);
@@ -8,5 +21,7 @@ PROCESS_THREAD(null_app_process, ev, data)
 {
   PROCESS_BEGIN();
   printf("Null App Started\n");
+  PRINTADDR(&rimeaddr_node_addr);
+  
   PROCESS_END();
 }
