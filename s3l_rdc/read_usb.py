1282d6fec71ff049aae88d2af3de3ce06fb20677
Yizheng Liao <liaoyizheng@gmail.com>
2013-07-03 00:41:41 -0700

    modify the read_sub.py to connect with mongoDB

--- s3l_rdc/read_usb.py
+++ s3l_rdc/read_usb.py
@@ -1,8 +1,28 @@
 import serial
 #import urllib
 #import urllib2
+import time;
+import datetime;
+
+import pymongo;
+from pymongo import MongoClient
 
 s = serial.Serial('/dev/ttyUSB0',115200,timeout=None)
+#s = 0
+client = MongoClient('54.215.3.34',27017)
+db = client['snowflake_ui_development']
+
+#data_input = {"sensor_id": 1,
+#	      "temperature": -100,
+#	      "timestamp": datetime.datetime.utcnow(),
+#	      "slot": 0}
+
+data_collection = db.data
+#post_id = data_collection.insert(data_input)
+#print post_id
+
+
+
 
 if not s.isOpen():
     s.open()
@@ -13,13 +33,21 @@ while True:
     if len(tmp) != 4:
         continue
     #url = 'http://128.12.167.67/sensorRead.php'
-    #values = {'Node'    : tmp[0],
-    #          'Packet'  : tmp[1],
-    #          'Time'    : tmp[2],
-    #          'Data'    : tmp[3]}
+    
     if tmp[3].endswith("\n"):
         tmp[3] =tmp[3][:-1]
+    localtime = time.asctime(time.localtime(time.time()))
+    print "Local Time:", localtime
     print tmp
+    
+    aa = tmp[3];
+    
+    values = {'sensor_id'    : int(float(tmp[0])),
+              'slot'  : int(tmp[1]),
+              'timestamp'    : datetime.datetime.utcnow(),
+              'temperature'    : int(aa[0:2])}
+    data_id = data_collection.insert(values)
+    
     f = open(tmp[0]+'.csv','a+')
     f.write(',')
     f.write(','.join(tmp[3].split(' ')))
