9937067f332b8bb8a7fd365cb6e26d1c6c6d612d
Mark <mgm1@stanford.edu>
2013-07-17 21:02:06 +0200

    Added test_mpu, a test handle for mpu_6050 accel and gyro raw data

--- Interface/test_mpu.c
+++ Interface/test_mpu.c
@@ -45,22 +45,24 @@
 #include "sys/etimer.h"
 #define debug
 #define DUR (2500)
-#define ADXL_ADDRESS 0xA6
-#define ITG_ADDRESS 0xD0
+#define MPU_ADDRESS 0xD0
+
 
   //unsigned char rv, xx0, xx1, yy0, yy1, zz0, zz1;
   int16_t accx, accy, accz;
   int16_t gyrx, gyry, gyrz;
   //float xx, yy, zz;
+
   
-void init_adxl();
-void init_itg();
-void set_measure();
-void measure_adxl();
-void measure_itg();
-void strange_measure();
-void start();
-void write(unsigned int _c);
+void init_mpu6050();
+void measure_mpu();
+
+//void set_measure();
+//void measure_adxl();
+//void measure_itg();
+//void strange_measure();
+//void start();
+//void write(unsigned int _c);
 /*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
 AUTOSTART_PROCESSES(&hello_world_process);
@@ -70,34 +72,16 @@ PROCESS_THREAD(hello_world_process, ev, data)
   PROCESS_BEGIN();
 	static struct etimer sensetimer;	
 	etimer_set(&sensetimer, CLOCK_SECOND);
+	uint8_t rv;
 	//WDTCTL = WDTPW+WDTHOLD;
 	// configuration -- bundle of single byte writes
 	#ifdef debug
 	printf("Configuring ADXL registers:\n");
 	#endif
-	init_adxl();
-	init_itg();
+	init_mpu6050();
+	
 	// Check the configuration registers:
 	
-	unsigned char configvector[29];
-	unsigned char startregister = 0x1D;
-	read_multibyte(ADXL_ADDRESS, startregister, 29, configvector);
-	int ii;
-	for(ii = 0; ii<29; ii++){
-		printf("%d register: %d \n", ii+startregister, configvector[ii]);
-	}
-
-	startregister = 0x15;
-	read_multibyte(ITG_ADDRESS, startregister, 3, configvector);
-	for(ii = 0; ii<3; ii++){
-		printf("%d register: %d \n", ii+startregister, configvector[ii]);
-	}
-
-	startregister = 0x3E;
-	unsigned char rv;
-	rv = read_(ITG_ADDRESS, startregister, 0);
-	printf("%d register: %d \n", startregister, rv);
-
 	while(1){
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&sensetimer));
 		etimer_reset(&sensetimer);
@@ -105,128 +89,66 @@ PROCESS_THREAD(hello_world_process, ev, data)
 		#ifdef debug
 		printf("Accessing WHOAMI register...\n");
 		#endif
-		rv = read_(ADXL_ADDRESS, 0x00, 0);
-		
-		if(rv == 229)
-			printf("i2c working correctly, this sensor is 229\n");
-		else{
-			printf("ERROR!ERROR!ERROR!ERROR!ERROR!ERROR!ERROR!\n");
-		}
+		rv = read_(MPU_ADDRESS, 0x75, 0);
+		printf("%d \n", rv);		
 
-		rv = read_(ITG_ADDRESS, 0x00, 0);
-		
-		printf("itg WHOAMI response: %d\n",rv);
-
-
-		// get acceleration on x, y, z axis
-
-		measure_adxl();
-		measure_itg();
-	
+		measure_mpu();
 
 		printf("Accel value: %d\tY value: %d\tZ value: %d\n", accx, accy, accz);
 
 		printf("Gyro value: %d\tY value: %d\tZ value: %d\n", gyrx, gyry, gyrz);
 
+
 	}
  	PROCESS_END();
 }
 
-
-void init_adxl(){
-	write_(ADXL_ADDRESS, 0x1D, 0x40);			
-	write_(ADXL_ADDRESS, 0x1E, 0x00);
-	write_(ADXL_ADDRESS, 0x1F, 0x00);
-	write_(ADXL_ADDRESS, 0x20, 0x00);		
-	write_(ADXL_ADDRESS, 0x21, 0x7f);
-	write_(ADXL_ADDRESS, 0x22, 0x30);		
-	write_(ADXL_ADDRESS, 0x23, 0x7f);
-	write_(ADXL_ADDRESS, 0x24, 0x02);		
-	write_(ADXL_ADDRESS, 0x25, 0x01);
-	write_(ADXL_ADDRESS, 0x26, 0xff);
-	write_(ADXL_ADDRESS, 0x27, 0xff);
-	write_(ADXL_ADDRESS, 0x28, 0x05);
-	write_(ADXL_ADDRESS, 0x29, 0x14);
-	write_(ADXL_ADDRESS, 0x2A, 0x07);
-	write_(ADXL_ADDRESS, 0x2B, 0x00);
-	//write_(ADXL_ADDRESS, 0x2C, 0x0A);//100 Hz sampling
-	write_(ADXL_ADDRESS, 0x2C, 0x07);//12.5 Hz sampling
-	write_(ADXL_ADDRESS, 0x2D, 0x08);		
-	write_(ADXL_ADDRESS, 0x2E, 0x00);		
-	write_(ADXL_ADDRESS, 0x31, 0x00);//not full_res, +- 2g
-//	write_(ADXL_ADDRESS, 0x31, 0x04);//full_res, +- 2g
-
+void init_mpu6050(){
+	write_(MPU_ADDRESS, 0x6B, 0x01); // clear sleep bit, set clock to 0x01 (x-gyro)
+	write_(MPU_ADDRESS, 0x1B, 0x00); // fs_250 for gyro
+	write_(MPU_ADDRESS, 0x1C, 0x00); // fs_2 for accel
 }
 
-
-void init_itg(){
-	write_(ITG_ADDRESS, 0x15, 0x07);			
-	write_(ITG_ADDRESS, 0x16, 0x19);			
-	write_(ITG_ADDRESS, 0x17, 0x00);			
-	write_(ITG_ADDRESS, 0x3E, 0x01);			
-}
 
 
 void set_measure(){
-	write_(ADXL_ADDRESS, 0x2D, 0x08);
 }
 
-void measure_adxl(){ // standard data accessing according to the datasheet
+void measure_mpu(){ 
 
-	unsigned char measurevector[6];
-	read_multibyte(ADXL_ADDRESS, 0x32, 6, measurevector);
-	//xx0 = measurevector[0];
-	//xx1 = measurevector[1];
-	//yy0 = measurevector[2];
-	//yy1 = measurevector[3];
-	//zz0 = measurevector[4];
-	//zz1 = measurevector[5];
+	unsigned char measurevector[14];
+	read_multibyte(MPU_ADDRESS, 0x3B, 14, measurevector);
 
-
-	//append the 2 bytes		
 	accx = 0;
-	accx |= measurevector[1];
-	accx = (accx<<8) | measurevector[0];
+	accx |= measurevector[0];
+	accx = (accx<<8) | measurevector[1];
 	
 	accy = 0;
-	accy |= measurevector[3];
-	accy = (accy<<8) | measurevector[2];
+	accy |= measurevector[2];
+	accy = (accy<<8) | measurevector[3];
 		
 	accz = 0;
-	accz |= measurevector[5];
-	accz = (accz<<8) | measurevector[4];
+	accz |= measurevector[4];
+	accz = (accz<<8) | measurevector[5];
 	
-	//printf("X: %d %d \nY: %d %d \nZ: %d %d \n",  xx0, xx1, yy0, yy1, zz0, zz1);
-}
-
-
-void measure_itg(){ 
-
-	unsigned char measurevector[8];
-	read_multibyte(ITG_ADDRESS, 0x1B, 8, measurevector);	
 
 	gyrx = 0;
-	gyrx |= measurevector[2];
-	gyrx = (gyrx<<8) | measurevector[3];
+	gyrx |= measurevector[8];
+	gyrx = (gyrx<<8) | measurevector[9];
 	
 	gyry = 0;
-	gyry |= measurevector[4];
-	gyry = (gyry<<8) | measurevector[5];
+	gyry |= measurevector[10];
+	gyry = (gyry<<8) | measurevector[11];
 		
 	gyrz = 0;
-	gyrz |= measurevector[6];
-	gyrz = (gyrz<<8) | measurevector[7];
+	gyrz |= measurevector[12];
+	gyrz = (gyrz<<8) | measurevector[13];
 	
 }
+
 
 void strange_measure(){ // non_standard reading. accessing each axis separately
 
-/*	xx0 = read_(ADXL_ADDRESS, 0x32, 0);
-	xx1 = read_(ADXL_ADDRESS, 0x33, 0);
-	yy0 = read_(ADXL_ADDRESS, 0x34, 0);
-	yy1 = read_(ADXL_ADDRESS, 0x35, 0);
-	zz0 = read_(ADXL_ADDRESS, 0x36, 0);		
-	zz1 = read_(ADXL_ADDRESS, 0x36, 0);*/
 }
 
 
