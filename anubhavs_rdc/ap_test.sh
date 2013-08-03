939f6a76f826cd603696e7fda252e3399efa5def
Yizheng Liao <liaoyizheng@gmail.com>
2013-08-03 15:22:17 -0700

    remove anubhavs' branch

--- anubhavs_rdc/ap_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ./test.cfg
-
-
-cp ./tdma_rdc_ap.c $CONTIKI_HOME/core/net/mac/tdma_rdc.c
-make nullApp.mspsim
-
--- anubhavs_rdc/sn_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ./test.cfg
-
-cp ./tdma_rdc_sn.c $CONTIKI_HOME/core/net/mac/tdma_rdc.c
-make nullApp.mspsim BASH_CONST=-DSN_ID=23
