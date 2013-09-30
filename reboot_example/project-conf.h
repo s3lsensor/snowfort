1348c08a690c59e5391d6342cfc7481f83729525
Yizheng Liao <liaoyizheng@gmail.com>
2013-09-29 23:47:46 -0700

    add example for rebooting mote remotely

---
+++ reboot_example/project-conf.h
@@ -0,0 +1,7 @@
+#define NETSTACK_CONF_RDC     tdmardc_driver
+
+//TODO: change mac protocol to TDMA mac protocol -- yzliao
+#define NETSTACK_CONF_MAC     nullmac_driver
+
+#define NETSTACK_CONF_FRAMER  framer_tdma
+
