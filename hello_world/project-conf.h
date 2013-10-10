241bbab3ab8532cbfd022aff2a56ead75f332e12
Yizheng Liao <liaoyizheng@gmail.com>
2013-10-09 18:08:19 -0700

    Revert to df3e44ba

--- hello_world/project-conf.h
+++ hello_world/project-conf.h
@@ -3,7 +3,4 @@
 //TODO: change mac protocol to TDMA mac protocol -- yzliao
 #define NETSTACK_CONF_MAC     nullmac_driver
 
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 4a08e08... Merge pull request #24 from s3lsensor/yzliao_v0.2_mac_hdr
