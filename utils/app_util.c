071db7db1610ba27e6e6b160d9d1b67caacac558
Yizheng Liao <liaoyizheng@gmail.com>
2014-04-08 20:09:36 -0700

    update util folder

--- utils/app_util.c
+++ utils/app_util.c
@@ -7,19 +7,8 @@
 
 #include "app_util.h"
 #include <stdio.h>
+//#include "platform-conf.h"
 
-#ifdef MSP430DEF_H
-int putint(int c)
-{
-  uart1_writeb((char)c);
-  return c;
-}
-
-void putchar(char c)
-{
-  uart1_writeb(c);
-}
-#endif
 
 /*
  * Function: app_output
@@ -27,18 +16,15 @@ void putchar(char c)
  * with the read_usb.py script. It is recommended for using this function to print
  * output instead of using the self-developed function.
  */
-void app_output(const char * data, const int node_id, const int pkt_seq, const int payload_len)
+void app_output(const uint8_t * data, const uint8_t node_id, const uint8_t pkt_seq, const uint8_t payload_len)
 {
-	printf("%u,%d",node_id,pkt_seq);
-	int i;
+  int i;
+
+  printf("%u,%u,",node_id,pkt_seq);
+
 	for(i = 0; i < payload_len; i++)
 	{
-#ifdef MSP430DEF_H
-	  putchar(',');
-	  putint(data[i]);
-#else
-	  printf(",%d",data[i]);
-#endif
+	  printf("%02x",data[i]);
 	}
 	printf("\n");
 }
--- utils/app_util.h
+++ utils/app_util.h
@@ -9,7 +9,8 @@
 #define APP_UTIL_H_
 
 //#include <stdio.h>
+#include "contiki.h"
 
-void app_output(const char * data, const int node_id, const int pkt_seq, const int payload_len);
+void app_output(const uint8_t * data, const uint8_t node_id, const uint8_t pkt_seq, const uint8_t payload_len);
 
 #endif /* APP_UTIL_H_ */
