51f63897eedb221a8020e2f8194bb53447ea807d
Yizheng Liao <liaoyizheng@gmail.com>
2014-05-21 22:58:21 -0700

    build.sh can take mote id as input variable

--- hello_world/build.sh
+++ hello_world/build.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
+MOTE_ID=$4
+: ${MOTE_ID=1}
 
 if [ "$1" = "make" ]; then
-#	cp $CONTIKI_HOME/s3l_rdc/tdmardc.h $CONTIKI_HOME/core/net/mac/tdmardc.h
-#	cp $CONTIKI_HOME/s3l_rdc/Makefile.mac $CONTIKI_HOME/core/net/mac/Makefile.mac
-#	cp $CONTIKI_HOME/s3l_rdc/tdmardc.c $CONTIKI_HOME/core/net/mac/tdmardc.c
-#	cp $CONTIKI_HOME/s3l_rdc/i2c.c $CONTIKI_HOME/platform/sky/dev/i2c.c
-#	cp $CONTIKI_HOME/s3l_rdc/i2c.h $CONTIKI_HOME/platform/sky/dev/i2c.h
     make clean
-	make $2.upload MOTE=1 BASH_CONST=-DSN_ID=$3
+	make $2.upload MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$3
 fi
 
 if [ "$1" = "test" ]; then
-#	cp $CONTIKI_HOME/s3l_rdc/tdmardc.h $CONTIKI_HOME/core/net/mac/tdmardc.h
-#	cp $CONTIKI_HOME/s3l_rdc/Makefile.mac $CONTIKI_HOME/core/net/mac/Makefile.mac
-#	cp $CONTIKI_HOME/s3l_rdc/tdmardc.c $CONTIKI_HOME/core/net/mac/tdmardc.c
-#	cp $CONTIKI_HOME/s3l_rdc/i2c.c $CONTIKI_HOME/platform/sky/dev/i2c.c
-#	cp $CONTIKI_HOME/s3l_rdc/i2c.h $CONTIKI_HOME/platform/sky/dev/i2c.h
     make clean
-	make $2.mspsim MOTE=1 BASH_CONST=-DSN_ID=$3
+	make $2.mspsim MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$3
 fi
 
 if [ "$1" = "login" ]; then
-	make login MOTE=1 | tee log_ap.txt
+	make login MOTE=$MOTE_ID | tee log_ap.txt
 fi
 
 if [ "$1" = "clean" ]; then
