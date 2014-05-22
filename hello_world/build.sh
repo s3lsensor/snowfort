a02f1518e538a48945e9d3cdd07b7480bccf1fc6
Yizheng Liao <liaoyizheng@gmail.com>
2014-05-22 00:32:59 -0700

    change build shell variable sequence

--- hello_world/build.sh
+++ hello_world/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-MOTE_ID=$4
+MOTE_ID=$2
 : ${MOTE_ID=1}
 
 if [ "$1" = "make" ]; then
     make clean
-	make $2.upload MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$3
+	make $3.upload MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$4
 fi
 
 if [ "$1" = "test" ]; then
     make clean
-	make $2.mspsim MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$3
+	make $3.mspsim MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$4
 fi
 
 if [ "$1" = "login" ]; then
-	make login MOTE=$MOTE_ID | tee log_ap.txt
+	make login MOTE=$MOTE_ID
 fi
 
 if [ "$1" = "clean" ]; then
