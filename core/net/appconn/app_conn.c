a98523a92cc71736255de5055f7a9b0967347df9
Yizheng Liao <liaoyizheng@gmail.com>
2014-08-30 15:57:26 -0700

    add counter for tdma rdc lock

--- core/net/appconn/app_conn.c
+++ core/net/appconn/app_conn.c
@@ -17,7 +17,7 @@
 #include "net/mac/tdmardc.h"
 #include "app_conn.h"
 
-#define DEBUG 0
+#define DEBUG 1
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
@@ -36,6 +36,8 @@ static volatile uint8_t tdma_rdc_buf_ptr = 0;
 static volatile uint8_t tdma_rdc_buf_send_ptr = 0;
 static volatile uint8_t tdma_rdc_buf_full_flg = 0;
 static volatile uint8_t tdma_rdc_buf_in_using_flg = 0;
+
+static uint8_t lock_counter = 0;
 
 /*---------------------------------------------------------------------------*/
 void app_conn_open(const struct app_callbacks *u)
@@ -60,10 +62,15 @@ void app_conn_send(const void * ptr, const uint16_t data_len)
 {
 
   //add guard for tdma_rdc_buffer access
-  while(tdma_rdc_buf_in_using_flg);
+  lock_counter = 0;
+  while(tdma_rdc_buf_in_using_flg && (lock_counter < 200))
+  {
+    lock_counter++;
+  }
 
   // lock tdma_rdc_buffer and preventing access from other functions.
   tdma_rdc_buf_in_using_flg = 1;
+
 
   if((data_len+tdma_rdc_buf_ptr) <= MAX_PKT_PAYLOAD_SIZE)
   {
@@ -97,7 +104,11 @@ void app_conn_send(const void * ptr, const uint16_t data_len)
 void tdma_rdc_buf_clear(void)
 {
   //add guard for tdma_rdc_buffer access
-  while(tdma_rdc_buf_in_using_flg);
+  lock_counter = 0;
+  while(tdma_rdc_buf_in_using_flg && (lock_counter < 200))
+  {
+    lock_counter++;
+  }
 
   // lock tdma_rdc_buffer and preventing access from other functions.
   tdma_rdc_buf_in_using_flg = 1;
@@ -118,7 +129,11 @@ void tdma_rdc_buf_clear(void)
 void tdma_rdc_buf_copyto_packetbuf(void)
 {
   //wait if the tdma_rdc_buffer is accessing by other functions
-  while(tdma_rdc_buf_in_using_flg);
+  lock_counter = 0;
+  while(tdma_rdc_buf_in_using_flg && (lock_counter < 200))
+  {
+    lock_counter++;
+  }
 
   // lock tdma_rdc_buffer and preventing access from other functions.
   tdma_rdc_buf_in_using_flg = 1;
