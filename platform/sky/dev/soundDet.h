47c68afa072ac79db0b69f6819cd780ee58ce8c0
consciousgaze <consciousgaze@gmail.com>
2014-12-30 21:45:00 -0800

    add more sensors

---
+++ platform/sky/dev/soundDet.h
@@ -0,0 +1,18 @@
+/*
+ * This file defines the driver for ms5803
+ *
+ * ---------------------------------------------------------
+ *
+ * Author: Bin Wang 
+ */
+
+/*
+ * Pin connection: analog output, need a adc converter
+
+ * usage: init before use to get the coefficients. reset everytime the sensor restarts. 
+ */
+#ifndef ML8511_H
+#define ML8511_H
+
+
+#endif /*ML8511_H*/
--- platform/sky/dev/tsl2561.h
+++ platform/sky/dev/tsl2561.h
@@ -16,29 +16,25 @@
 
  * usage: init before use to get the coefficients. reset everytime the sensor restarts. 
  */
-#ifndef MS5803_H
-#define MS5803_H
+#ifndef TSL2561_H
+#define TSL2561_H
 
 #define delay_1ms()   do{ _NOP(); _NOP(); _NOP(); _NOP(); \
                           _NOP(); _NOP(); _NOP(); _NOP(); \
                           _NOP(); _NOP(); }while(250)
 
-#define CMD_RESET 0x1E // reset command
-#define CMD_ADC_READ 0x00 // ADC read command
-#define CMD_ADC_CONV 0x40 // ADC conversion command
-#define CMD_PROM 0xA0 // Coefficient location
+#define TSL2561_ADDR   0x39
 
-
-#define PRESSURE 0x00
-#define TEMPERATURE 0x10
-
-// precisions
-#define ADC_256  0x00
-#define ADC_512  0x02
-#define ADC_1024  0x04
-#define ADC_2048  0x06
-#define ADC_4096  0x08
-
+#define TSL2561_CMD           0x80
+#define TSL2561_CMD_CLEAR     0xC0
+#define	TSL2561_REG_CONTROL   0x00
+#define	TSL2561_REG_TIMING    0x01
+#define	TSL2561_REG_THRESH_L  0x02
+#define	TSL2561_REG_THRESH_H  0x04
+#define	TSL2561_REG_INTCTL    0x06
+#define	TSL2561_REG_ID        0x0A
+#define	TSL2561_REG_DATA_0    0x0C
+#define	TSL2561_REG_DATA_1    0x0E
 
 #define SWAP(a,b) a = a^b; b = a^b; a = a^b;
 #define MPU_PRINT_BYTE( X ) (uart1_writeb((unsigned char)X));
@@ -63,14 +59,15 @@ typedef struct
 	int32_t temperature;
 }ms5803_union
 
-#define MS5803_DATA_SIZE (sizeof(ms5803_data)/sizeof(uint8_t))
+// #define MS5803_DATA_SIZE (sizeof(ms5803_data)/sizeof(uint8_t))
 
-int16_t* ms5803_init();
-int ms5803_reset();
-int ms5803_send(int8_t cmd);
-int ms5803_sample(int8_t precision, int16_t *coeff);
+int16_t* tsl2561_init();
+int tsl2561_poweron();
+int tsl2561_powerdonw(int8_t cmd);
+int tsl2561_settiming();
+int tsl2561_sample(int8_t precision, int16_t *coeff);
 
 
-void print_ms5803_sample(ms5803_union samples);
+void print_tsl2561_sample(ms5803_union samples);
 
 #endif
