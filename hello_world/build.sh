c56399c2dda55fd77271bcbea26d12e7877f4885
Instant Contiki <user@instant-contiki.(none)>
2015-03-24 10:17:10 -0700

    Modify. Makefile - Makefile modified so that sensors are added via build.sh - append the sensor part numbers to build.sh to include the sensor - e.g. sudo ./build.sh make 1 example 3 htu21d isl29125 to use htu21d and isl29125

--- hello_world/build.sh
+++ hello_world/build.sh
@@ -4,8 +4,8 @@ MOTE_ID=$2
 : ${MOTE_ID=1}
 
 if [ "$1" = "make" ]; then
-    make clean
-	make $3.upload MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$4
+	make clean
+	make sensors="$(echo $(tr '[:upper:]' '[:lower:]' <<< "${@:5}") | sed 's/ /.c /g').c" $3.upload MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$4
 fi
 
 if [ "$1" = "test" ]; then
