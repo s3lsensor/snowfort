8d024c2becc628ecf17a59970ac11f073a145941
Instant Contiki <user@instant-contiki.(none)>
2015-04-03 01:07:49 -0700

    Add. sensor check - add sensor checking in build.sh to ensure the user input valid sensor part number

--- hello_world/build.sh
+++ hello_world/build.sh
@@ -5,6 +5,16 @@ MOTE_ID=$2
 
 if [ "$1" = "make" ]; then
 	make clean
+	for i in $(echo $(tr '[:upper:]' '[:lower:]' <<< "${@:5}") | sed 's/ /.c /g').c; do
+		if [ -e ../platform/sky/dev/$i ]
+		then
+			echo ../platform/sky/dev/$i
+		else
+			echo ../platform/sky/dev/$i
+			echo "$i does not exists, wrong sensor part number?"
+			exit
+		fi 
+	done
 	make sensors="$(echo $(tr '[:upper:]' '[:lower:]' <<< "${@:5}") | sed 's/ /.c /g').c" $3.upload MOTE=$MOTE_ID BASH_CONST=-DSN_ID=$4
 fi
 
